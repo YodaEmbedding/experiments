55ef25f001603f1516704be64225726ef53e358f
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-05-27 00:32:57 -0700

    Bit more verilog intro

--- verilog/test.v
+++ verilog/test.v
@@ -1,9 +1,44 @@
 
+// TODO difference between =, <=, and assign?
+// =      "blocking"
+// <=     "non-blocking"
+// assign "concurrent" (no always or sensitivity list required)
+
+module AdderStage(a, b, cin, sum, cout);
+	input a, b, cin;
+	output sum, cout;
+
+	// reg sum, cout;
+
+	always @ (a or b or cin) begin
+		sum <= a ^ b ^ cin;
+		cout <= (a & b) | (a & cin) | (b & cin);
+	end
+endmodule
+
+module adder8(a, b, cin, sum, cout);
+	input[7:0] a, b;
+	input cin;
+	output[7:0] sum;
+	output cout;
+	wire[8:0] carry;
+
+	carry[0] <= cin;
+
+	// always? not needed?
+
+	for (i=0; i < 8; i++)
+		AdderStage(a[i], b[i], carry[i], sum[i], carry[i + 1]);
+
+	cout <= carry[8];
+endmodule
+
 module NotBit(x, y);
 	input x;
 	output y;
 	// wire s;
 
+	// posedge clk?
 	y <= not x;
 endmodule
 
@@ -11,13 +46,9 @@ module NotByte(xs, ys);
 	input[7:0] xs;
 	output[7:0] ys;
 
-	for ?
-	ys
-
-
-	for loop?
-
-	// and (out, a, b);
+	// posedge clk?
+	for (i = 0; i < 8; i = i + 1)
+		NotBit(x[i], y[i]);
 endmodule
 
 module DLatch(clk, D, Q);
@@ -33,4 +64,7 @@ always @(posedge clk)
 	...
 
 always @(select A or B)
+
+
+.notation?
 
