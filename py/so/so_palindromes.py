982a7574ff47323e8f8e06a2dd37681f2cde5c95
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-06-09 20:12:53 -0700

    Miscellaneous

---
+++ py/so/so_palindromes.py
@@ -0,0 +1,10 @@
+import os
+
+def is_palindrome(x):
+    return x == x[::-1]
+
+with open('so_palindromes.txt', 'r') as f:
+    for line in map(lambda x: x.rstrip(), f):
+        if is_palindrome(line):
+            print(line)
+
