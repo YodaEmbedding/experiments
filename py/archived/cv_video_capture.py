846a01a55d38da6fc9ac5d4695b589f99459b303
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-07-02 20:19:29 -0700

    OpenCV video capture device test

---
+++ py/archived/cv_video_capture.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(ret)
+    print(frame if frame is None else frame.shape)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
