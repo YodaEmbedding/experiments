805d3b1063544bcf61f4871178e0e2a8400ca71c
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-08-22 21:44:38 -0700

    Monad notes/summary

--- hs/prelude.hs
+++ hs/prelude.hs
@@ -1,19 +1,34 @@
 import Prelude ((.), ($), (++), id)
 import System.IO
 
+-- -- Summary:
+-- fmap   :: (a -> b) -> m a   -> m b     -- Map function over container
+-- (>>=)  :: m a -> (a -> m b) -> m b     -- Map function over container
+-- join   :: m (m a)           -> m a     -- Reduces   container "dimension"
+-- return :: a                 -> m a     -- Increases container "dimension"
+--
+-- -- These may be defined in terms of each other:
+-- fmap   f xs = xs >>= (return . f)
+-- (>>=)  xs f = (join . fmap) f xs
+-- join   xss  = xss >>= id
+-- return x    = (undefinable)
+--
+-- -- Monad Laws:
+-- return x   >>= f       ≡  f x
+-- xs         >>= return  ≡  xs
+-- (xs >>= f) >>= g       ≡  xs >>= (\x -> f x >>= g)
+
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
 
 class (Functor m) => Monad m where
   return :: a -> m a
   (>>=) :: m a -> (a -> m b) -> m b
-  -- join :: m (m a) -> m a
 
 data Maybe a = Just a | Nothing
 
 instance Functor Maybe where
-  fmap fn (Just x) = Just $ fn x
-  fmap fn Nothing  = Nothing
+  fmap f x = x >>= (return . f)
 
 instance Monad Maybe where
   return = Just
