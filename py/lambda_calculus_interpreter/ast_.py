729e45ba3cc077a9f126ee809df7b6cc46448da8
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-09-24 01:10:56 -0700

    Cleanup/reorganize

--- py/lambda_calculus_interpreter/ast_.py
+++ py/lambda_calculus_interpreter/ast_.py
@@ -1,6 +1,24 @@
+from dataclasses import dataclass
 
+@dataclass
+class AppNode:
+    lhs: object
+    rhs: object
 
-# Abstraction
-# Application
-# Identifier
+    def __str__(self):
+        return f'{self.lhs} {self.rhs}'
 
+@dataclass
+class AbsNode:
+    name: object
+    body: object
+
+    def __str__(self):
+        return f'(λ{self.name}.{self.body})'
+
+@dataclass
+class IdNode:
+    name: str
+
+    def __str__(self):
+        return self.name
