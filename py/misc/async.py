3530018247a195acdf6cdd0d95a1d0dc7fac57a5
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-11-25 01:19:59 -0800

    asyncio; stackoverflow iterator question

---
+++ py/misc/async.py
@@ -0,0 +1,46 @@
+import asyncio
+import random
+import time
+
+async def io_bound(x):
+    print(f'Task {x} started')
+    dt = random.uniform(0.1, 1.0)
+    t = time.time()
+    await asyncio.sleep(dt)
+    elapsed = time.time() - t
+    print(f'Task {x} await took {elapsed:.6f} versus expected {dt:.6f}')
+    return x
+
+def create_coroutines(n=3):
+    return [io_bound(x) for x in range(n)]
+
+# class Collector:
+#     def __init__(self, tasks):
+#         self.tasks = tasks
+#
+#     async def __anext__(self):
+
+async def main():
+    tasks = [asyncio.create_task(x) for x in create_coroutines()]
+
+    # Guaranteed processing order
+    for task in tasks:
+        x = await task
+        print(f'Processed {x}')
+
+    print('')
+
+    tasks = [asyncio.create_task(x) for x in create_coroutines()]
+
+    # Guaranteed processing order
+    # async for task in tasks:
+    #     x = await task
+    #     print(f'Processed {x}')
+
+    # for result in await asyncio.gather(*tasks):
+    #     print(result)
+
+    results = await asyncio.gather(*tasks)
+    print(results)
+
+asyncio.run(main())
---
+++ py/so/so_iterator_cycle.py
@@ -0,0 +1,18 @@
+class CyclicIterable:
+    def __init__(self, data):
+        self._data = list(data)
+
+    def __iter__(self):
+        while True:
+            yield from self._data
+
+    def __next__(self):
+        yield from self.__iter__()
+
+cycle = CyclicIterable(['a', 'b', 'c', 'd'])
+for i, x in zip(range(5), cycle):
+    print(x)
+
+it = cycle.__iter__()
+for i, x in zip(range(5), it):
+    print(x)
