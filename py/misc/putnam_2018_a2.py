e494179ab7c42c6254b77c331b327b3495146321
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-12-01 23:03:13 -0800

    Fairly memory efficient. Still could be reduced by a big amount if numpy would only perform determinants with int8s...

--- py/misc/putnam_2018_a2.py
+++ py/misc/putnam_2018_a2.py
@@ -1,4 +1,4 @@
-from itertools import chain, combinations, product
+from itertools import chain, combinations
 import numpy as np
 np.set_printoptions(threshold=np.nan)
 
@@ -9,28 +9,39 @@ def powerset(iterable):
     return sorted(ps, key=lambda x: (len(x), *x))
 
 def cartesian_self_product(a):
-    xs = np.repeat(a[np.newaxis, :, :], a.shape[0], axis=0)
-    ys = np.repeat(a[:, np.newaxis, :], a.shape[0], axis=1)
-    return xs, ys
-    # return np.meshgrid(xs, xs)
+    return np.meshgrid(a, a)
 
 def I(a, b):
-    return np.any(a & b, axis=-1)
+    return (a & b) > 0
 
-def encode_bitvector(xs, n):
-    enc = np.zeros((len(xs), n), dtype=np.int)
+def encode_bitvector(xs, n, dtype):
+    enc = np.zeros((len(xs), n), dtype=np.uint8)
     for i, x in enumerate(xs):
         enc[i, x] = 1
-    return enc
+    bits = np.packbits(enc, axis=-1)
+    return bits.view(dtype)
 
-I_vec = np.vectorize(I)
+def get_dtype(n):
+    return (
+        np.uint8  if n <= 8  else
+        np.uint16 if n <= 16 else
+        np.uint32 if n <= 32 else
+        np.uint64 if n <= 64 else None)
 
-for n in range(2, 17):
-    xs = powerset(range(n))
-    ps_enc = encode_bitvector(xs, n)
+for n in range(2, 14):
+    dtype = get_dtype(n)
+    ps = powerset(range(n))
+    ps_enc = encode_bitvector(ps, n, dtype)
     prod = cartesian_self_product(ps_enc)
-    M = I(*prod).astype(np.int64)
+    M = I(*prod).astype(dtype)
     det = int(np.linalg.det(M))
     # print(M)
+    # print(M.shape)
     # print(f'Power set: {ps}')
     print(f'n={n} determinant: {det}')
+
+# NOTE 2  GB required for n=12
+# NOTE 4  GB required for n=13
+# NOTE 8  GB required for n=14
+# NOTE 16 GB required for n=15
+# NOTE 32 GB required for n=16
