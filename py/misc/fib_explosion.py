c1f495c0328ea3a5c4d93b323974a1804bebda1b
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-01-20 03:44:29 -0800

    Example of combinatorial explosion of unmemoized recursive function that calls itself twice

---
+++ py/misc/fib_explosion.py
@@ -0,0 +1,16 @@
+# from functools import lru_cache
+
+calls = 0
+
+# @lru_cache()
+def fib(n):
+    global calls
+    calls += 1
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+print(fib(32))
+print(f'Number of calls to fib() required: {calls}')
