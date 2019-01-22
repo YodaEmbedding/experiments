ec0d40e6ea555d97e4b8c16db7f94cf2df82a454
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-01-22 06:55:40 -0800

    Almost there

--- fortran/phys395_hw1/plot.py
+++ fortran/phys395_hw1/plot.py
@@ -19,8 +19,10 @@ def read_csv(filename):
 
 def plot_csv(csv_filename, out_filename, ylim, title):
     header, rows = read_csv(csv_filename)
-    it = zip(header, zip(*rows))
-    _, x = next(it)
+    series = list(map(np.array, zip(*rows)))
+    x, f, f10, f100 = series
+    it = zip(header, series)
+    _, _ = next(it)
 
     plt.figure()
     for i, (label, data) in enumerate(it):
@@ -31,7 +33,25 @@ def plot_csv(csv_filename, out_filename, ylim, title):
     plt.title(title)
     plt.savefig(out_filename, dpi=300)
 
+    err10  = np.abs(f - f10)
+    err100 = np.abs(f - f100)
+
+    with np.warnings.catch_warnings():
+        np.warnings.filterwarnings('ignore')
+        print('{:12} | {:12.3f} | {:12.3f} | {:12.3f} | {:12.3f}'.format(
+            out_filename.lstrip('plot_').rstrip('.png'),
+            np.max(err10),
+            x[np.argmax(err10)],
+            np.max(err100),
+            x[np.argmax(err100)]))
+
 def main():
+    print(
+        '\nThe max and argmax of the error is provided below for both'
+        '\nChebyshev polynomial with 10 terms and'
+        '\nChebyshev polynomial with 100 terms.\n')
+    print('{:12} | {:>12} | {:>12} | {:>12} | {:>12}'.format('Name', 'max err10', 'argmax err10', 'max err100', 'argmax err100'))
+
     plot_csv(
         csv_filename='results_f_uniform.csv',
         out_filename='plot_f_uniform.png',
@@ -56,4 +76,9 @@ def main():
         ylim=(-4.0, 4.0),
         title=r'$\frac{d}{dx}f(x)$ evaluated for $x_i$ at zeros of $T_n(x)$')
 
+    print()
+
 main()
+
+# TODO plot out error
+# TODO figure out why results are backwards for "_zeros"
