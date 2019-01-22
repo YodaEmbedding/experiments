187d8dde1a8f4d8a11b53835deb8b06a58808774
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-01-22 02:53:29 -0800

    Arbitrary number of plotted sample points

--- fortran/phys395_hw1/phys395_hw1_orthogonal_polynomials.f90
+++ fortran/phys395_hw1/phys395_hw1_orthogonal_polynomials.f90
@@ -2,54 +2,63 @@ program orthogonal_polynomials
   use gauss_jordan
   implicit none
 
-  integer, parameter :: fh = 1
-  integer, parameter :: m = 100  ! Size of sampled point set
-  integer, parameter :: n = 10  ! Size of basis set
-  real, dimension(m) :: x       ! x_i
-  real, dimension(m) :: f_x     ! Actual function evaluated at x_i
-  real, dimension(m) :: y_x     ! Approximated function evaluated at x_i
-  real, dimension(m, n) :: B_x  ! Basis functions evaluated at x_i
-  real, dimension(n) :: c       ! Coefficients
-  integer :: i, j
+  integer, parameter :: fh = 1    ! File handle
+  integer, parameter :: m = 10    ! Size of sampled point set
+  integer, parameter :: n = 10    ! Size of basis set
+  real, dimension(m) :: x         ! x_i
+  real, dimension(m) :: f_x       ! Actual function evaluated at x_i
+  real, dimension(m) :: y_x       ! Approx function evaluated at x_i
+  real, dimension(m, n) :: B_x    ! Basis functions evaluated at x_i
+  real, dimension(m, n) :: B_x_T  ! Transpose of B_x
+  real, dimension(n) :: c         ! Coefficients
+  integer :: i
   character(len=32) :: fmt_str
 
   x = linspace(-1.0, 1.0, m)
   f_x = f(x)
   B_x = chebyshevT_matrix(n, x)
-  ! c = solve(B_x, f_x)
-  c = solve(matmul(transpose(B_x), B_x), matmul(transpose(B_x), f_x))
-  ! TODO move least squares into gauss-jordan directly? nah
+  B_x_T = transpose(B_x)
 
+  ! Naively, one can perform c = solve(B_x, f_x).
+  ! However, this does not work when m != n.
+  ! Instead, one can use the linear least squares idea of multiplying by
+  ! the transpose of B_x first in order to properly match the dimensions.
+  c = solve(matmul(B_x_T, B_x), matmul(B_x_T, f_x))
 
-  write (fmt_str, "(a, i5, a)"), "(", n, "f8.2)"
+  write (fmt_str, "(a, i5, a)") "(", n, "f8.2)"
   print fmt_str, transpose(B_x)
   print *
   print fmt_str, c
 
-  ! forall (i=1:m) y_x(i) = chebyshevT_poly(c, x(i))
-  print fmt_str, c
-  forall (i=1:m) y_x(i) = chebyshevT_poly(c, x(i))
-
-  ! character(len=32) :: fmt_str
-  ! write (fmt_str, "(a,i3,a)") "(f8.4, ", m - 1, "(',', f8.4))"
-  ! print *, fmt_str
-  ! print "(f8.4)", x
-  ! print "(f8.4)", f_x
-
   open(unit=fh, file="results.csv", action="write", status="replace")
   write(fh, "(a)") "x, f(x), f_approx(x)"
-
-  ! TODO m should be 10, but we plot a higher resolution result by computing it on a different set of x_i values
-  ! That is, we should loop through 1, samples or something like that
-  do i = 1, m
-    write(fh, "(f8.4, a, f8.4, a, f8.4)") x(i), ", ", f_x(i), ", ", y_x(i)
-  end do
-
+  call write_csv_chebyshevT(fh, c, 100)
   close(fh)
 
 contains
 
-  !! TODO docstring
+  !! Write a plottable csv of f(x) and chebyshevT using given coefficients
+  subroutine write_csv_chebyshevT(fh, coeffs, num_samples)
+    implicit none
+
+    integer, intent(in) :: num_samples
+    integer, intent(in) :: fh
+    real, dimension(:), intent(in) :: coeffs
+    real, dimension(num_samples) :: x     ! x_i
+    real, dimension(num_samples) :: f_x   ! Actual function evaluated at x_i
+    real, dimension(num_samples) :: y_x   ! Approx function evaluated at x_i
+    integer :: i
+
+    x = linspace(-1.0, 1.0, num_samples)
+    f_x = f(x)
+    forall (i=1:num_samples) y_x(i) = chebyshevT_poly(coeffs, x(i))
+
+    do i = 1, num_samples
+      write(fh, "(f8.4, a, f8.4, a, f8.4)") x(i), ", ", f_x(i), ", ", y_x(i)
+    end do
+  end subroutine
+
+  !! Create the matrix Bij = chebyshevT(j, x_i)
   pure function chebyshevT_matrix(n, x)
     real, dimension(:), intent(in) :: x
     integer, intent(in) :: n
@@ -58,14 +67,13 @@ contains
     forall (i=1:n) chebyshevT_matrix(:, i) = chebyshevT(i - 1, x)
   end function
 
-  !! TODO docstring
+  !! Chebyshev polynomial with coefficients c evaluated at x
   pure function chebyshevT_poly(c, x)
     real, dimension(:), intent(in) :: c
     real, intent(in) :: x
     real :: chebyshevT_poly
     integer :: i
 
-    ! chebyshevT_poly = sum((/(c(i) * chebyshevT(i - 1, x), i=1,size(c))/))
     chebyshevT_poly = sum(c * chebyshevT((/(i, i=0,size(c)-1)/), x))
   end function
 
