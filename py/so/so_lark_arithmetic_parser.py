07912e1c50949c21d05d2b418d1cde7777d327ef
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-01-28 00:01:03 -0800

    Generate parse tree for arithmetic expressions (and right-assosciative for exponents)

---
+++ py/so/so_lark_arithmetic_parser.py
@@ -0,0 +1,28 @@
+from lark import Lark
+
+# NOTE This doesn't properly handle right-assosciative expressions like "4^5^6"
+parser = Lark('''
+    ?sum: product
+        | sum "+" product       -> add
+        | sum "-" product       -> sub
+
+    ?product:
+        | product "*" exponent  -> mul
+        | product "/" exponent  -> div
+        | exponent
+
+    ?exponent:
+        | item "^" exponent     -> exp
+        | item
+
+    ?item: NUMBER               -> number
+        | "-" item              -> neg
+        | "(" sum ")"
+
+    %import common.NUMBER
+    %import common.WS
+    %ignore WS''', start='sum')
+
+s = '4 * 2 ^ 3 + 4 ^ 1'
+tree = parser.parse(s)
+print(tree.pretty())
