42119ca50b1002588a38c9f427053ff2f858b388
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-02-05 04:14:03 -0800

    Solve coefficients

--- fortran/phys395_hw2/phys395_hw2_curve_fitting.f90
+++ fortran/phys395_hw2/phys395_hw2_curve_fitting.f90
@@ -4,13 +4,13 @@ program phys395_hw2_curve_fitting
   implicit none
   integer, parameter :: ifh = 1, ofh = 2
   integer, parameter :: lines = 9130  ! TODO: allow arbitrary lengths
-  character(len=*), parameter :: filename = "out.csv"
+  character(len=*), parameter :: filename = "results_.csv"
   integer :: a, i, j, k, stat
   real :: x, y
   real, dimension(lines) :: xs, ys, ys_fit
   integer, parameter :: n = 3
   real, dimension(n + 1, lines) :: bax
-  real, dimension(n + 1, n + 1) :: B
+  real, dimension(n + 1, n + 1) :: B, B_
   real, dimension(n + 1) :: coeffs
   real, dimension(n + 1) :: p
   character(len=64) :: fmt_str
@@ -28,18 +28,36 @@ program phys395_hw2_curve_fitting
 
   forall (i=1:lines, a=0:n) bax(a + 1, i) = basis(a, xs(i))
   forall (j=1:n+1, k=1:n+1) B(j, k) = sum(bax(j, : ) * bax(k, : ))
-  ! forall (j=1:n+1) p(j) = sum(bax(j, :) * ys(:))
   p = matmul(bax, ys)
 
+  B_ = B
+  coeffs = p
+  call solve(n + 1, B_, coeffs, -1.0)
+
   write(fmt_str, "(a, i10, a)") "(", n+1, "f10.2)"
+  print *, "B"
   print fmt_str, B
+  print *, "p"
   print fmt_str, p
+  print *, "coeffs"
+  print fmt_str, coeffs
 
   open(unit=ofh, file=filename, action="write", status="replace")
   write(ofh, "(a)") "testing"
   close(ofh)
 
 contains
+
+  ! minimize |A.x - B|^2 using LAPACK canned SVD routine (A gets destroyed, the answer is returned in B)
+  ! rcond determines the effective rank of A as described in LAPACK docs. Pass -1.0 for machine precision
+  subroutine solve(n, A, B, rcond)
+    integer :: n
+    real :: A(n, n), B(n), S(n), W(6 * n), rcond
+    integer :: rank, stat
+    stat = 0 ! TODO removable?
+    call dgelss(n, n, 1, A, n, B, n, S, rcond, rank, W, 6 * n, stat)
+    if (stat /= 0) call abort
+  end subroutine
 
   !! An even basis... from a more civilized age
   elemental function basis(a, x)
