9b6569068d4926dac34804bfed14bc80f567c69d
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-02-05 00:15:29 -0800

    Possible solution to HW2... correctness TBD

--- fortran/phys395_hw2/phys395_hw2.py
+++ fortran/phys395_hw2/phys395_hw2.py
@@ -4,7 +4,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fftpack import fft
+# from scipy.fftpack import fft
 
 plt.style.use('dark_background')
 np.set_printoptions(precision=2)
@@ -12,13 +12,42 @@ np.set_printoptions(precision=2)
 def basis(a, x):
     return np.cos(2 * np.pi * a * x)
 
-def get_coeffs(x, y, num_coeffs):
-    idxs = np.arange(num_coeffs)
+def condition_number(a):
+    _, s, _ = np.linalg.svd(a)
+    return np.max(s) / np.min(s)
+
+def construct_arrays(x, y, n):
+    idxs = np.arange(n + 1)
     bxa = basis(*np.meshgrid(idxs, x, sparse=True))
     B = np.einsum('ij,ik->jk', bxa, bxa)
     p = np.einsum('ij,i->j', bxa, y)
-    coeffs = np.linalg.solve(B, p)
-    return coeffs, bxa
+    return B, p, bxa
+
+def solve(B, p):
+    # coeffs = np.linalg.solve(B, p)
+    # coeffs = svd_solve(B, p)
+    coeffs, _, _, _ = np.linalg.lstsq(B, p, rcond=None)
+    return coeffs
+
+def svd_solve(a, b):
+    u, s, vh = np.linalg.svd(a)
+    return vh.T.dot(np.diag(1. / s).dot(u.T.dot(b)))
+
+def fit(x, y, n):
+    B, p, bxa = construct_arrays(x, y, n)
+    coeffs = solve(B, p)
+    y_fit = np.dot(bxa, coeffs)
+
+    print(f'n={n}')
+    print('\nCoefficients of basis function cos(2pi a x):')
+    print(coeffs)
+    print('\nCondition number:')
+    print(condition_number(B))
+    print('\nChi square:')
+    print(np.sum((y - y_fit)**2))
+    print('\n----\n')
+
+    return y_fit
 
 def main():
     with open('data.dat', 'r') as f:
@@ -26,23 +55,24 @@ def main():
 
     data = np.array(data)
     x, y = data.T
-    N = data.shape[0]
-    T = (x[-1] - x[0]) / N
+    # N = data.shape[0]
+    # T = (x[-1] - x[0]) / N
+    # yf = fft(y)
+    # xf_ = np.linspace(0.0, 0.5 / T, N // 2)
+    # yf_ = 2.0 / N * np.abs(yf[0:N//2])
 
-    yf = fft(y)
-    xf_ = np.linspace(0.0, 0.5 / T, N // 2)
-    yf_ = 2.0 / N * np.abs(yf[0:N//2])
-
-    coeffs, bxa = get_coeffs(x, y, 4)
-    y_fit = np.dot(bxa, coeffs)
-    print('Coefficients of basis function cos(2pi a x):')
-    print(coeffs)
+    y_fit3 = fit(x, y, n=3)
+    y_fit7 = fit(x, y, n=7)
 
     fig, axes = plt.subplots(nrows=2, sharex=False)
     axes[0].plot(x, y)
-    axes[0].plot(x, y_fit)
-    axes[1].plot(xf_[:16], yf_[:16])
-    axes[1].set_yscale('log')
+    axes[0].plot(x, y_fit3, label='n=3')
+    axes[1].plot(x, y)
+    axes[1].plot(x, y_fit7, label='n=7')
+    # axes[1].plot(xf_[:16], yf_[:16])
+    # axes[1].set_yscale('log')
+    axes[0].legend()
+    axes[1].legend()
     fig.savefig('plot.png', dpi=300)
 
 main()
