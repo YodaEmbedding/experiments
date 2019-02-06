b6e4542cfb94864b879ed6444099e6f195b279a3
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-02-05 23:01:18 -0800

    More progress

--- fortran/phys395_hw2/phys395_hw2_curve_fitting.f90
+++ fortran/phys395_hw2/phys395_hw2_curve_fitting.f90
@@ -7,14 +7,9 @@ program phys395_hw2_curve_fitting
   integer, parameter :: ifh = 1, ofh = 2
   integer, parameter :: lines = 9130  ! TODO: allow arbitrary lengths
   character(len=*), parameter :: filename = "results_.csv"
-  integer :: a, i, j, k, stat
+  integer ::  i, stat
   real :: x, y
-  real, dimension(lines) :: xs, ys, ys_fit
-  integer, parameter :: n = 3
-  real, dimension(n + 1, lines) :: bax
-  real, dimension(n + 1, n + 1) :: B, U, Vh
-  real, dimension(n + 1) :: coeffs, p, s
-  character(len=64) :: fmt_str
+  real, dimension(lines) :: xs, ys, ys_fit3, ys_fit7
 
   open(unit=ifh, file="data.dat", action="read", status="old", &
     access="sequential", form="formatted")
@@ -27,35 +22,69 @@ program phys395_hw2_curve_fitting
     if (stat < 0) exit
   end do
 
-  forall (i=1:lines, a=0:n) bax(a + 1, i) = basis(a, xs(i))
-  forall (j=1:n+1, k=1:n+1) B(j, k) = sum(bax(j, :) * bax(k, :))
-  p = matmul(bax, ys)
-
-  call svd(n + 1, B, U, s, Vh)
-
-  ! coeffs = solve(B, p)
-  ! coeffs = solve_svd(n + 1, p, U, s, Vh, 1.0e-6)
-  coeffs = solve_lss(n + 1, B, p, -1.0)
-
-  write(fmt_str, "(a, i10, a)") "(", n + 1, "f10.2)"
-  print *, "B"
-  print fmt_str, B
-  print *, "p"
-  print fmt_str, p
-  print *, "coeffs"
-  print fmt_str, coeffs
-  print *, "U"
-  print fmt_str, U
-  print *, "s"
-  print fmt_str, s
-  print *, "Vh"
-  print fmt_str, Vh
+  print *
+  ys_fit3 = fit(xs, ys, 3, "svd")
+  ys_fit7 = fit(xs, ys, 7, "svd")
 
   open(unit=ofh, file=filename, action="write", status="replace")
-  write(ofh, "(a)") "testing"
+  write(ofh, *) "x, y, $f_3(x)$, $f_7(x)$"
+  do i=1, lines
+    write(ofh, *) xs(i), ", ", ys(i), ", ", ys_fit3(i), ", ", ys_fit7(i)
+  end do
   close(ofh)
 
 contains
+
+  function fit(xs, ys, n, solver) result(ys_fit)
+    integer :: a, i, j, k, n
+    real, dimension(:) :: xs, ys
+    real, dimension(n + 1) :: ys_fit, coeffs, p, s
+    real, dimension(n + 1, size(xs, 1)) :: bax
+    real, dimension(n + 1, n + 1) :: B, U, Vh
+    real :: cond_num, chi_actual, chi_expect
+    character(len=64) :: fmt_str
+    character(len=*) :: solver
+
+    ! Construct B and p to solve the linear equation Bc = p
+    forall (a=0:n, i=1:size(bax, 2)) bax(a + 1, i) = basis(a, xs(i))
+    forall (j=1:n+1, k=1:n+1) B(j, k) = sum(bax(j, :) * bax(k, :))
+    p = matmul(bax, ys)
+
+    call svd(n + 1, B, U, s, Vh)
+
+    select case (solver)
+    case ("gj");  coeffs = solve(B, p)
+    case ("svd"); coeffs = solve_svd(n + 1, p, U, s, Vh, 1.0e-6)
+    case ("lss"); coeffs = solve_lss(n + 1, B, p, -1.0)
+    end select
+
+    ys_fit = matmul(transpose(bax), coeffs)
+    chi_actual = sum((ys - ys_fit)**2)
+    chi_expect = size(xs, 1) - (n + 1)
+    cond_num = s(1) / s(n + 1)
+
+    write(fmt_str, "(a, i10, a)") "(", n + 1, "f7.2)"
+    print "(a, a)",  "solver = ", solver
+    print "(a, i1)", "n      = ", n
+    print *
+    ! print "(a)", "B"
+    ! print fmt_str, B
+    ! print "(a)", "U"
+    ! print fmt_str, U
+    ! print "(a)", "s"
+    ! print fmt_str, s
+    ! print "(a)", "Vh"
+    ! print fmt_str, Vh
+    ! print "(a)", "p"
+    ! print fmt_str, p
+    print "(a)", "coeffs"
+    print fmt_str, coeffs
+    print *
+    print "(a21, f10.2)", "Condition number: ",    cond_num
+    print "(a21, f10.2)", "Chi-square (actual): ", chi_actual
+    print "(a21, f10.2)", "Chi-square (expect): ", chi_expect
+    print "(/, a, /)", "----"
+  end function
 
   !! minimize |A.x - B|^2 using LAPACK canned SVD routine
   !! A gets destroyed, the answer is returned in B
@@ -115,6 +144,5 @@ contains
 
 end program phys395_hw2_curve_fitting
 
-! TODO ew n + 1 everywhere
-! TODO Good fit: chi^2 = number of points - number of params fit
-! (Chi-square should be on average 1, since sigma is 1)
+! TODO rename xs, ys; x_val, y_val?
+! TODO " (Hint: Your matrix A will have different dimensions.)"
