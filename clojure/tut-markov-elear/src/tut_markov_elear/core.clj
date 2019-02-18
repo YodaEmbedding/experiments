4552b98c9313b2ecc14fdca7a90e4408dfc37a34
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-02-17 23:27:46 -0800

    n smallest elements in original order (heapq solution)

---
+++ clojure/tut-markov-elear/src/tut_markov_elear/core.clj
@@ -0,0 +1,4 @@
+(ns tut-markov-elear.core)
+
+(defn -main [& args]
+  (println "Hello, world"))
---
+++ py/codewars/n_smallest_elements_in_original_order.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# https://www.codewars.com/kata/n-smallest-elements-in-original-order/train/python
+
+import heapq
+import operator
+
+def first_n_smallest(arr, n):
+    pairs = zip(arr, range(0, len(arr)))
+    h = heapq.nsmallest(n, pairs)
+    h.sort(key=operator.itemgetter(1))
+    return [x for x, i in h]
