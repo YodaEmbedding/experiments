613eea70031201f5068511cb2eb4d0277a76063f
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-03-12 01:09:37 -0700

    Color

--- fortran/phys395_hw4/plot.py
+++ fortran/phys395_hw4/plot.py
@@ -36,7 +36,6 @@ def plot_time_series(csv_filename, out_filename, ylim=None, title=None):
     fig.savefig(out_filename, dpi=300)
 
 def plot_trajectory(csv_filename, out_filename, xlim=None, ylim=None, title=None):
-    # TODO color map changing line? over time?
     header, rows = read_csv(csv_filename)
     series = list(map(np.array, zip(*rows)))
     t, th1, th2, energy = series
@@ -47,23 +46,28 @@ def plot_trajectory(csv_filename, out_filename, xlim=None, ylim=None, title=None
     x2 =  l2 * np.sin(th2) + x1
     y2 = -l2 * np.cos(th2) + y1
 
-    fig, axes = plt.subplots(nrows=1)
-    axes = [axes]
-    # axes[0].plot(x2, y2)
-    plot_cmapped(fig, axes[0], x2, y2)
+    cmaps = [plt.cm.viridis, plt.cm.magma]
+    legend_elements = [
+        plt.Line2D([0], [0], color=cmaps[0](0.7), lw=2, label=header[1]),
+        plt.Line2D([0], [0], color=cmaps[1](0.7), lw=2, label=header[2])]
 
-    axes[0].set_title(title)
-    axes[0].set_xlim(xlim)
-    axes[0].set_ylim(ylim)
-    axes[0].legend(framealpha=0.9)
+    fig, ax = plt.subplots(nrows=1)
+    plot_cmapped(fig, ax, t, x2, y2, cmaps[1])
+    plot_cmapped(fig, ax, t, x1, y1, cmaps[0])
+
+    ax.set_title(title)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_aspect('equal')
+    ax.legend(handles=legend_elements, framealpha=0.9)
     fig.savefig(out_filename, dpi=300)
 
-def plot_cmapped(fig, ax, x, y):
+def plot_cmapped(fig, ax, t, x, y, cmap='viridis'):
     points = np.array([x, y]).T.reshape(-1, 1, 2)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    t = np.linspace(0.0, 1.0, x.shape[0])
+    # t = np.linspace(0.0, 1.0, x.shape[0])
     norm = plt.Normalize(t.min(), t.max())
-    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    lc = LineCollection(segments, cmap=cmap, norm=norm)
     lc.set_array(t)
     lc.set_linewidth(2)
     line = ax.add_collection(lc)
@@ -79,12 +83,12 @@ def plot_multiple(ax, x, it):
 def main():
     plot_time_series(
         csv_filename='results.csv',
-        out_filename='plot_time_series.png',
+        out_filename='plot_time_series.svg',
         title=r'Double pendulum')
 
     plot_trajectory(
         csv_filename='results.csv',
-        out_filename='plot_trajectory.png',
+        out_filename='plot_trajectory.svg',
         title=r'Double pendulum',
 		xlim=(-2.0, 2.0),
 		ylim=(-2.0, 2.0))
