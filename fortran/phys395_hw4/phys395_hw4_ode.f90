33b682fd29721e5325e0fe838cf4eada24474872
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-03-14 03:00:31 -0700

    Extract integrator module

--- fortran/phys395_hw4/phys395_hw4_ode.f90
+++ fortran/phys395_hw4/phys395_hw4_ode.f90
@@ -2,11 +2,10 @@
 
 program phys395_hw4_ode
   use fitsio
+  use integrator
   implicit none
 
   real, parameter :: pi = 3.1415926535897932384626433832795028841971693993751058
-  real, parameter :: m1 = 1.0, m2 = 1.0
-  real, parameter :: l1 = 1.0, l2 = 1.0
   real :: energy_flip_min
 
   energy_flip_min = min( &
@@ -18,7 +17,6 @@ program phys395_hw4_ode
 
 contains
 
-  ! TODO enforce real(8) for floating point computations for this question
   subroutine q2()
     integer, parameter :: steps_per_second = 2**8 ! TODO change this
     real, parameter :: time_period = 100.0 * sqrt(1.0 / 9.806)
@@ -30,7 +28,7 @@ contains
     print *
     print "(a)", "Q2. Plot energy violation and create video animation"
     print "(a)", "    Initial conditions: theta_1 = pi / 3, theta_2 = -pi/3"
-    print "(a, f9.7)", "    dt: ", dt
+    print "(a, f9.7, a)", "    dt: ", dt, "s"
     print *
     call run(steps, dt, ts, ys, y0=[pi / 3, 0.0, -pi / 3, 0.0])
     call write_csv("results.csv", steps, ts, ys)
@@ -44,7 +42,7 @@ contains
     real, parameter :: dt = 1.0 / steps_per_second
     integer, parameter :: steps = steps_per_second * time_period
 
-    print "(a)", "Q3. Fractal output"
+    print "(a)", "Q3. Fractal generation"
     print *
     call write_fractal("plot_fractal_16x16.fit",     dt, steps, [-pi, pi], [-pi, pi], 16, 16)
     call write_fractal("plot_fractal_32x32.fit",     dt, steps, [-pi, pi], [-pi, pi], 32, 32)
@@ -117,7 +115,7 @@ contains
     !! Write phase plot within ranges given by th1 and th2
     character(len=*) :: filename
     character(len=256) :: cmd
-    integer, parameter :: iters_since_msg_max = 512
+    integer, parameter :: iters_since_msg_max = 1024
     integer :: steps, width, height, i, j, iters_since_msg
     real :: dt, th1(2), th2(2), data_(1, width, height)
 
@@ -155,111 +153,6 @@ contains
     print "(2a)", "Written output to ", filename
     print *
   end subroutine
-
-  function evalf(y) result(dydt)
-    real, intent(in) :: y(4)
-    real, parameter :: g = 9.806
-    real :: dydt(4), m, c, s, s1, s2, scale1, scale2
-
-    associate(th1 => y(1), w1 => y(2), th2 => y(3), w2 => y(4))
-      m = m1 + m2
-      c = cos(th2 - th1)
-      s = sin(th2 - th1)
-      s1 = sin(th1)
-      s2 = sin(th2)
-      scale1 = m * l1 - m2 * l1 * c * c
-      scale2 = (l2 / l1) * scale1
-
-      dydt(1) = w1
-
-      dydt(2) = ( &
-        (m2 * l1 * w1**2 * s * c) + &
-        (m2 * l2 * w2**2 * s) +     &
-        (m2 * g * s2 * c) +         &
-        (-m * g * s1)) / scale1
-
-      dydt(3) = w2
-
-      dydt(4) = ( &
-        (-m2 * l2 * w2**2 * s * c) + &
-        (-m  * l1 * w1**2 * s) +     &
-        ( m * g * s1 * c) +          &
-        (-m * g * s2)) / scale2
-    end associate
-  end function
-
-  pure function energy(y)
-    real, intent(in) :: y(4)
-    real, parameter :: g = 9.806
-    real :: energy, T, V, y1, y2
-
-    associate(th1 => y(1), w1 => y(2), th2 => y(3), w2 => y(4))
-      y1 = -l1 * cos(th1) + 2.0
-      y2 = -l2 * cos(th2) + y1
-
-      T = 0.5 * ( &
-        m1 * l1**2 * w1**2 + &
-        m2 * ( &
-          l1**2 * w1**2 + &
-          l2**2 * w2**2 + &
-          2 * l1 * l2 * w1 * w2 * cos(th2 - th1)))
-      V = g * (m1 * y1 + m2 * y2)
-    end associate
-
-    energy = T + V
-  end function
-
-  ! 10th order implicit Gauss-Legendre integrator
-  subroutine gl10(y, dt)
-    integer, parameter :: s = 5, n = 4
-    real y(n), g(n, s), dt
-    integer i, k
-
-    ! Butcher tableau for 10th order Gauss-Legendre method
-    real, parameter :: a(s, s) = reshape([ &
-      0.5923172126404727187856601017997934066Q-1,  &
-      -1.9570364359076037492643214050884060018Q-2, &
-      1.1254400818642955552716244215090748773Q-2,  &
-      -0.5593793660812184876817721964475928216Q-2, &
-      1.5881129678659985393652424705934162371Q-3,  &
-      1.2815100567004528349616684832951382219Q-1,  &
-      1.1965716762484161701032287870890954823Q-1,  &
-      -2.4592114619642200389318251686004016630Q-2, &
-      1.0318280670683357408953945056355839486Q-2,  &
-      -2.7689943987696030442826307588795957613Q-3, &
-      1.1377628800422460252874127381536557686Q-1,  &
-      2.6000465168064151859240589518757397939Q-1,  &
-      1.4222222222222222222222222222222222222Q-1,  &
-      -2.0690316430958284571760137769754882933Q-2, &
-      4.6871545238699412283907465445931044619Q-3,  &
-      1.2123243692686414680141465111883827708Q-1,  &
-      2.2899605457899987661169181236146325697Q-1,  &
-      3.0903655906408664483376269613044846112Q-1,  &
-      1.1965716762484161701032287870890954823Q-1,  &
-      -0.9687563141950739739034827969555140871Q-2, &
-      1.1687532956022854521776677788936526508Q-1,  &
-      2.4490812891049541889746347938229502468Q-1,  &
-      2.7319004362580148889172820022935369566Q-1,  &
-      2.5888469960875927151328897146870315648Q-1,  &
-      0.5923172126404727187856601017997934066Q-1], [s, s])
-
-    real, parameter :: b(s) = [ &
-      1.1846344252809454375713202035995868132Q-1, &
-      2.3931433524968323402064575741781909646Q-1, &
-      2.8444444444444444444444444444444444444Q-1, &
-      2.3931433524968323402064575741781909646Q-1, &
-      1.1846344252809454375713202035995868132Q-1]
-
-    g = 0.0
-    do k = 1, 16
-      g = matmul(g, a)
-      do i = 1, s
-        g(:, i) = evalf(y + g(:, i) * dt)
-      end do
-    end do
-
-    y = y + matmul(g, b) * dt
-  end subroutine gl10
 
 end program phys395_hw4_ode
 
