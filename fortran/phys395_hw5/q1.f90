04d1bd31341bc08e61f2294e6c2d8ea58afd9222
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-04-01 20:44:45 -0700

    Generalize write_csv

--- fortran/phys395_hw5/q1.f90
+++ fortran/phys395_hw5/q1.f90
@@ -25,7 +25,7 @@ contains
     call integrate_ode_bidirectional(steps, dt, ys, y0)
     print "(a, f9.7)", "    N:       ", integrate_sum(dt, ys)
     print *
-    call write_csv("results.csv", bi_steps, ys)
+    call write_csv("results.csv", ys, "$x$, $\psi(x)$, $\psi'(x)$")
     call execute_command_line("python plot.py results.csv --plot-results")
     print *
 
@@ -59,19 +59,28 @@ contains
   !   bi_steps = 2 * steps - 1
   ! end function
 
-  subroutine write_csv(filename, n, y)
+  subroutine write_csv(filename, mat, header, num_fmt)
     !! Output a csv file with plottable data
     integer, parameter :: ofh = 2
-    character(len=64), parameter :: fmt_str = &
-      "(ES32.16, ',', ES32.16, ',', ES32.16)"
-    character(len=*) :: filename
-    integer :: n, i
-    real :: y(nn, n)
+    real :: mat(:, :)
+    character(len=*) :: filename, header
+    character(len=*), optional :: num_fmt
+    character(:), allocatable :: num_fmt_, fmt_str
+    integer :: i
+
+    if (      present(num_fmt)) num_fmt_ = num_fmt
+    if (.not. present(num_fmt)) num_fmt_ = "ES32.16"
+
+    fmt_str = "(" // num_fmt_
+    do i = 2, size(mat, 1)
+      fmt_str = fmt_str // ", ',', " // num_fmt_
+    end do
+    fmt_str = fmt_str // ")"
 
     open(unit=ofh, file=filename, action="write", status="replace")
-    write(ofh, *) "$x$, $\psi(x)$, $\psi'(x)$"
-    do i = 1, n
-      write(ofh, fmt_str) y(1, i), y(2, i), y(3, i)
+    write(ofh, *) header
+    do i = 1, size(mat, 2)
+      write(ofh, fmt_str) mat(:, i)
     end do
     close(ofh)
   end subroutine
