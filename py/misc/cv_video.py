bbb86e01dd213e87c6a788d0298831ba7517b894
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-11-07 02:59:30 -0800

    OpenCV video writer demo

---
+++ py/misc/cv_video.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+height = 500
+width = 700
+gray = np.zeros((height, width), dtype=np.uint8)
+
+# fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+# filename = "output.avi"
+fourcc = cv2.VideoWriter_fourcc(*"MP4V")
+filename = "output.mp4"
+writer = cv2.VideoWriter(
+    filename, fourcc, fps=30, frameSize=(width, height), isColor=False
+)
+# NOTE isColor doesn't seem to influence resulting file size
+
+xs = np.arange(width // 10)
+ys = np.arange(height // 10)
+locations = np.dstack(np.meshgrid(ys, xs)).reshape(-1, 2)
+for y, x in locations:
+    gray[y, x] = 255
+    # gray_3c = cv2.merge([gray, gray, gray])
+    writer.write(gray)
+
+writer.release()
