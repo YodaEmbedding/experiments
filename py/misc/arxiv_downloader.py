7a4347a77d560c53a788f5c686ae09d7a048f884
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-06-29 02:56:53 -0700

    arxiv downloader/file renamer now corrects metadata via exiftool

--- py/misc/arxiv_downloader.py
+++ py/misc/arxiv_downloader.py
@@ -1,32 +1,53 @@
-import os, re, sys
+import os
+import re
+import subprocess
+import sys
+
 import arxiv
+
+
+def fix_title(title: str) -> str:
+    return " ".join(map(str.strip, title.split("\n")))
+
 
 def paper_to_filename(paper: dict) -> str:
     authors = paper["authors"]
-    title_str = " ".join(map(str.strip, paper["title"].split("\n")))
+    title_str = fix_title(paper["title"])
     author_str = authors[0] if len(authors) == 0 else f"{authors[0]} et al."
     filename = f"{author_str} - {title_str}"
-
-    print(f"{filename}.pdf")
-    print(f"url:     {paper['arxiv_url']}")
-    print(f"author:  {paper['author']}")   # "main" author
-    print(f"authors: {paper['authors']}")  # list of all authors
-    print(f"title:   {paper['title']}\n")
     return filename
+
 
 def parse_line(line: str):
     id_pattern = r"(\d{4}\.\d{4,6}(v\d+)?)"
     m = re.match(fr".*{id_pattern}(\.pdf)?$", line)
     return m.group(1) if m is not None else None
 
+
 paper_ids = [parse_line(line.strip()) for line in sys.stdin.readlines()]
 paper_ids = [x for x in paper_ids if x is not None]
 papers = arxiv.query(id_list=paper_ids)
 
 for paper, paper_id in zip(papers, paper_ids):
-    if os.path.exists(f"{paper_id}.pdf"):
-        print("[Renamed]")
-        os.rename(f"{paper_id}.pdf", f"{paper_to_filename(paper)}.pdf")
+    basename = paper_to_filename(paper)
+    dst_filename = f"{basename}.pdf"
+    src_filename = f"{paper_id}.pdf"
+    if os.path.exists(src_filename):
+        print(f"[Rename] {src_filename}")
+        os.rename(src_filename, dst_filename)
     else:
-        print("[Downloaded]")
-        arxiv.download(paper, slugify=paper_to_filename)
+        print("[Download]")
+        arxiv.download(paper, slugify=lambda _, basename=basename: basename)
+    print(dst_filename)
+    print(f"url:     {paper['arxiv_url']}")
+    print(f"author:  {paper['author']}")
+    print(f"authors: {paper['authors']}")
+    print(f"title:   {paper['title']}\n")
+    args = [
+        "exiftool",
+        dst_filename,
+        "-overwrite_original",
+        f"-Author={'; '.join(paper['authors'])}",
+        f"-Title={fix_title(paper['title'])}",
+    ]
+    subprocess.run(args, capture_output=True, check=True)
