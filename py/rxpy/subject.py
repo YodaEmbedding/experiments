22f1e96d4bbbf56aac0805f6e38b03d1db2e0c55
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-11-12 04:36:24 -0800

    py: rxpy/subject

---
+++ py/rxpy/subject.py
@@ -0,0 +1,17 @@
+import rx
+import rx.subject
+
+def test_subject(subject):
+    disposable = subject.subscribe(print)
+    subject.on_next("Hello, world!")
+    subject.on_next("After this line, I am gone.")
+    disposable.dispose()
+    subject.on_next("I am no more!")
+
+
+def main():
+    subject = rx.subject.Subject()
+    test_subject(subject)
+
+
+main()
