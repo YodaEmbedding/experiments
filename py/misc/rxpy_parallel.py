f8acac937e3bab8547ef4d22e2a7c789c0b5b9f6
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-11-12 03:44:31 -0800

    py: rxpy_parallel

--- py/misc/rxpy_parallel.py
+++ py/misc/rxpy_parallel.py
@@ -11,13 +11,22 @@ def print_thread(tag):
         print(tag, x, current_thread().name)
     return inner
 
-optimal_thread_count = multiprocessing.cpu_count() + 1
-pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
+
+pool_scheduler = ThreadPoolScheduler(multiprocessing.cpu_count())
+
 rx.from_iterable(range(10)).pipe(
+    # Runs on main thread
     ops.do_action(print_thread(0)),
+
+    # Switch threads to unused one from pool
     ops.flat_map(
         lambda x: rx.just(x, pool_scheduler).pipe(
             ops.do_action(print_thread(1)),
         )
     ),
-).subscribe(print_thread(2), CurrentThreadScheduler())
+
+    # This executes on the same thread from the pool as in the flat_map
+    ops.do_action(print_thread(2)),
+).subscribe()
+
+print("This may be printed on any line *after* the final MainThread print!")
