c547eff0b4a81f4a85ecf7b97f7814349e92f422
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-09-13 17:10:26 -0700

    py: update arxiv_downloader for arxiv==1.4.2

--- py/misc/arxiv_downloader.py
+++ py/misc/arxiv_downloader.py
@@ -7,7 +7,7 @@ import arxiv
 
 
 def fix_title(title: str) -> str:
-    title = " ".join(map(str.strip, title.split("\n")))
+    title = " ".join(str.strip(x) for x in title.split("\n"))
     title = title.replace(":", "_")
     return title
 
@@ -16,7 +16,7 @@ def paper_to_filename(paper: arxiv.Result) -> str:
     authors = paper.authors
     title_str = fix_title(paper.title)
     author_str = authors[0] if len(authors) == 1 else f"{authors[0]} et al."
-    filename = f"{author_str} - {title_str}"
+    filename = f"{author_str} - {title_str}.pdf"
     return filename
 
 
@@ -40,24 +40,23 @@ def set_metadata(filename: str, title: str, author: str):
 lines = sys.stdin.readlines()
 paper_ids = [parse_line(line.strip()) for line in lines]
 paper_ids = [x for x in paper_ids if x is not None]
-papers = arxiv.Search(id_list=paper_ids).get()
+papers = arxiv.Search(id_list=paper_ids).results()
 
 for paper, paper_id in zip(papers, paper_ids):
-    basename = paper_to_filename(paper)
-    dst_filename = f"{basename}.pdf"
     src_filename = f"{paper_id}.pdf"
+    dst_filename = paper_to_filename(paper)
     if os.path.exists(src_filename):
         print(f"[Rename] {src_filename}")
         os.rename(src_filename, dst_filename)
     else:
         print("[Download]")
-        arxiv.download(paper, slugify=lambda _, basename=basename: basename)
-    print(dst_filename)
+        paper.download_pdf(filename=dst_filename)
+    print(f"file:    {dst_filename}")
     print(f"url:     {paper.entry_id}")
-    print(f"authors: {list(map(str, paper.authors))}")
+    print(f"authors: {[str(x) for x in paper.authors]}")
     print(f"title:   {paper.title}\n")
     set_metadata(
         dst_filename,
         title=fix_title(paper.title),
-        author="; ".join(map(str, paper.authors)),
+        author="; ".join(str(x) for x in paper.authors),
     )
