5b22cbceea02c8b71670c91ef15aea6fb64c6f20
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-03-21 18:57:49 -0700

    js: lichess server analysis response handling

--- js/misc/lichessRequestAnalysis.js
+++ js/misc/lichessRequestAnalysis.js
@@ -1,4 +1,6 @@
-const RATE_LIMIT = 1200 * 1000;
+const ANALYZE_WAIT = 300 * 1000;
+const DAILY_LIMIT_WAIT = 12 * 3600 * 1000;
+const RATE_LIMIT = 5 * 1000;
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -11,22 +13,51 @@ const requestAnalysis = gameId => fetch(
     },
     method: "post",
   }
-).then(response => {
-  const text = response.text();
-  console.log(text);
-});
+);
 
 const parseGameIdFromUrl = url => (
-  url.match(/^https?:\/\/lichess.org\/([a-zA-Z0-9]+)\/?.*$/)[1]
+  url.match(/^https?:\/\/lichess.org\/([a-zA-Z0-9]{8})\/?.*$/)[1]
 );
+
+const tryAnalyseGameId = async gameId => {
+  console.log(`Requesting analysis for https://lichess.org/${gameId} ...`);
+  const response = await requestAnalysis(gameId);
+  const text = await response.text();
+  console.log(response);
+  console.log(text);
+
+  if (text === "This game is already analysed") {
+    await delay(RATE_LIMIT);
+    return "success";
+  }
+
+  if (text === "You already have an ongoing requested analysis") {
+    await delay(Math.max(RATE_LIMIT, ANALYZE_WAIT));
+    return "try_again";
+  }
+
+  if (
+    text === "You have reached the weekly analysis limit" ||
+    text === "You have reached the daily analysis limit" ||
+    text === "You have reached the daily analysis limit on this IP"
+  ) {
+    await delay(Math.max(RATE_LIMIT, DAILY_LIMIT_WAIT));
+    return "try_again";
+  }
+
+  await delay(Math.max(RATE_LIMIT, ANALYZE_WAIT));
+  return "success";
+};
 
 const requestAnalysisForUrls = async gameUrls => {
   for (let i = 0; i < gameUrls.length; i++) {
     const gameUrl = gameUrls[i];
     const gameId = parseGameIdFromUrl(gameUrl);
-    console.log(`Requesting analysis for ${gameId}...`);
-    await requestAnalysis(gameId);
-    await delay(RATE_LIMIT);
+
+    let result = "try_again";
+    while (result === "try_again") {
+      result = await tryAnalyseGameId(gameId);
+    }
   }
 }
 
