8bf7274a849ee0356ab491dcb40dc1bf01932a0c
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-01 17:27:55 -0700

    js(lichess): extract urls by parsing PGN to dicts

---
+++ js/lichess/extract_urls_from_pgn.py
@@ -0,0 +1,47 @@
+import re
+import sys
+
+
+def parse_pgn(lines):
+    game = {}
+
+    for line in lines:
+        line = line.rstrip("\n")
+
+        if line == "":
+            if "Moves" in game:
+                yield game
+                game = {}
+            continue
+
+        match = re.search(r'^\[(?P<key>\w+) "(?P<value>.*)"\]$', line)
+
+        if match:
+            game[match.group("key")] = match.group("value")
+            continue
+
+        match = re.match(r"^(\d+\.)|0-1|1-0|1/2-1/2", line)
+
+        if match:
+            if "Moves" not in game:
+                game["Moves"] = ""
+            game["Moves"] += f"{line}\n"
+            continue
+
+        raise Exception("Invalid line: {}".format(line))
+
+
+def main():
+    filename = sys.argv[1]
+
+    with open(filename) as f:
+        for game in parse_pgn(f):
+            if "%eval" in game["Moves"]:
+                continue
+            if re.match(r".*\b(Casual|Bullet|UltraBullet)\b", game["Event"]):
+                continue
+            print(game["Site"])
+
+
+if __name__ == "__main__":
+    main()
