1c6a5c4641b9a38efc3303edfc956c29e461de5d
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-01-07 22:53:27 -0800

    bash: ffmpeg_video_info

---
+++ bash/ffmpeg_video_info.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# See also: https://superuser.com/questions/1106343/determine-video-bitrate-using-ffmpeg
+
+IGNORE_EXTENSIONS=(jpg jpeg png webp heic txt)
+
+for f in *; do
+  # Skip directories.
+  if [[ -d "$f" ]]; then
+    continue
+  fi
+
+  # Skip files with ignored extensions.
+  extension="${f##*.}"
+  # NOTE: A bit hacky.
+  if [[ " ${IGNORE_EXTENSIONS[@]} " =~ " ${extension} " ]]; then
+    continue
+  fi
+
+  # WARNING: The output order of the entries doesn't seem to depend on the specified order of the stream keys.
+  info="$(ffprobe -v quiet -select_streams v:0 -show_entries stream=codec_name,duration,bit_rate -of default=noprint_wrappers=1:nokey=1 "$f")"
+
+  codec_name="$(echo "$info" | cut -d$'\n' -f1)"
+  duration="$(echo "$info" | cut -d$'\n' -f2)"
+  bit_rate="$(echo "$info" | cut -d$'\n' -f3)"
+
+  printf '%5s %12s %12s %s\n' "$codec_name" "$bit_rate" "$duration" "$f"
+done
+
