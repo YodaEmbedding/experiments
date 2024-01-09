515ccbb860567e69e834ea0eef4e3edf474c60da
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-01-08 20:51:03 -0800

    py: random_3x3_rotation_matrix revisited

--- py/mathy/random_3x3_rotation.py
+++ py/mathy/random_3x3_rotation.py
@@ -17,8 +17,18 @@ def qr_full(num_samples=1):
     return rot_z_maybe @ qr_half(num_samples)
 
 
+# https://math.stackexchange.com/questions/442418/random-generation-of-rotation-matrices/4394036#4394036
+# https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d/44701#44701
+def randn_axis(num_samples=1):
+    z = np.random.randn(num_samples, 1, 3)
+    z = z / np.linalg.norm(z, axis=-1, keepdims=True)
+    angle = np.random.uniform(0, 2 * np.pi, size=num_samples)
+    return rot3x3_from_axis_angle(z, angle)
+
+
 # https://math.stackexchange.com/questions/442418/random-generation-of-rotation-matrices/442423#442423
-def nbubis_incorrect(num_samples=1):
+# https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d/44691#44691
+def nbubis(num_samples=1):
     u1 = np.random.uniform(0, 1, size=num_samples)
     u2 = np.random.uniform(0, 1, size=num_samples)
     u3 = np.random.uniform(0, 1, size=num_samples)
@@ -34,51 +44,6 @@ def nbubis_incorrect(num_samples=1):
     return rot3x3_from_axis_angle(axis_vector, psi)
 
 
-# Doesn't really work.
-def nbubis_repaired(num_samples=1):
-    u1 = np.random.uniform(0, 1, size=num_samples)
-    u2 = np.random.uniform(0, 1, size=num_samples)
-    u3 = np.random.uniform(0, 1, size=num_samples)
-    theta = np.arccos(2 * u1 - 1)
-    phi = 2 * np.pi * u2
-    psi = (1 - np.cos(np.pi * u3)) / np.pi
-    axis_vector = [
-        np.sin(theta) * np.cos(phi),
-        np.sin(theta) * np.sin(phi),
-        np.cos(theta),
-    ]
-    axis_vector = np.stack(axis_vector, axis=1).reshape(-1, 1, 3)
-    # return axis_vector.swapaxes(-1, -2).repeat(3, axis=-1)  # Peek at vector.
-    return rot3x3_from_axis_angle(axis_vector, psi)
-
-    # cs = np.cos(psi)
-    # ss = np.sin(psi)
-    # ct = np.cos(theta)
-    # st = np.sin(theta)
-    # cp = np.cos(phi)
-    # sp = np.sin(phi)
-
-    # return np.array(
-    #     [
-    #         [
-    #             cs * cp - ct * ss * sp,
-    #             ss * cp + ct * cs * sp,
-    #             st * sp,
-    #         ],
-    #         [
-    #             -cs * sp - ct * ss * cp,
-    #             -ss * sp + ct * cs * cp,
-    #             st * cp,
-    #         ],
-    #         [
-    #             ss * st,
-    #             -cs * st,
-    #             ct,
-    #         ],
-    #     ]
-    # ).transpose((2, 0, 1))
-
-
 # https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula#Matrix_notation
 def rot3x3_from_axis_angle(axis_vector, angle):
     angle = np.atleast_1d(angle)[..., None, None]
@@ -86,28 +51,49 @@ def rot3x3_from_axis_angle(axis_vector, angle):
     return np.eye(3) + np.sin(angle) * K + (1 - np.cos(angle)) * (K @ K)
 
 
-def plot_scatter(points, filename):
+# Scipy method. Seems to be the same as the above.
+# def rot3x3_from_axis_angle(axis_vector, angle):
+#     from scipy.spatial.transform import Rotation
+#
+#     angle = np.atleast_1d(angle)[..., None]
+#     axis_vector = axis_vector.squeeze(1)
+#     axis_vector = axis_vector / np.linalg.norm(axis_vector, axis=-1, keepdims=True)
+#     rot = Rotation.from_rotvec(angle * axis_vector)
+#     return rot.as_matrix()
+
+
+def plot_scatter(pointses, filename, kwargses):
     fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter(*points.T, marker=".", s=1.0)
-    ax.view_init(elev=45, azim=-135, roll=0)
+    ax = fig.add_subplot(projection="3d", computed_zorder=False)
+    for points, kwargs in zip(pointses, kwargses):
+        ax.scatter(*np.asarray(points).T, marker=".", **kwargs)
+    ax.view_init(elev=45, azim=-45, roll=0)
     ax.set(xlim=(-1, 1), ylim=(-1, 1), zlim=(-1, 1))
     ax.set_aspect("equal", adjustable="box")
     fig.savefig(filename, dpi=300, bbox_inches="tight", pad_inches=0)
-    plt.show()
+    # plt.show()
+    # exit()
     plt.close(fig)
 
 
 METHODS = {
     "qr_half": qr_half,
     "qr_full": qr_full,
-    "nbubis_incorrect": nbubis_incorrect,
-    "nbubis_repaired": nbubis_repaired,
+    "randn_axis": randn_axis,
+    "nbubis": nbubis,
 }
 
 
+# x is the starting point; y contains various sample rotated points.
+x = np.array([1.0, 0.0, 0.0])
+x = np.array([1 / 9, -4 / 9, 8 / 9])
+x /= np.linalg.norm(x)  # Normalize to unit vector, just in case.
+
 for name, func in METHODS.items():
     rot = func(num_samples=5000 // (2 if "_half" in name else 1))
-    x = np.array([1, 0, 0])
     y = rot @ x
-    plot_scatter(y, f"rot3x3_{name}.png")
+    plot_scatter(
+        [y, [x]],
+        f"rot3x3_{name}.png",
+        [{"s": 1, "alpha": 0.5}, {"s": 64, "color": "#ff77cc"}],
+    )
