94655af13eb4fbe73e879b8054c4af3ef1dfa844
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-01-25 05:06:44 -0800

    bash: ffmpeg_video_info (filesize too)

--- bash/ffmpeg_video_info.sh
+++ bash/ffmpeg_video_info.sh
@@ -25,7 +25,8 @@ for f in *; do
   height="$(echo "$info" | cut -d$'\n' -f3)"
   duration="$(echo "$info" | cut -d$'\n' -f4)"
   bit_rate="$(echo "$info" | cut -d$'\n' -f5)"
+  file_size="$(du -h "$f" | cut -f1)"
 
-  printf '%5s %12s %4sx%4s %12s %s\n' "$codec_name" "$bit_rate" "$width" "$height" "$duration" "$f"
+  printf '%5s %12s %4sx%4s %6s %12s %s\n' "$codec_name" "$bit_rate" "$width" "$height" "$file_size" "$duration" "$f"
 done
 
