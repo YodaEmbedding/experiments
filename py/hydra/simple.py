6ef9ede2ebf9e2ac6a61d24a9ad0080af020c37a
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-09-19 13:58:42 -0700

    py: hydra

--- py/hydra/simple.py
+++ py/hydra/simple.py
@@ -1,10 +1,12 @@
 import hydra
-from omegaconf import DictConfig
+import yaml
+from omegaconf import DictConfig, OmegaConf
 
 
 @hydra.main(version_base=None, config_path="conf")
 def main(conf: DictConfig):
-    print(conf)
+    # print(conf)
+    print(yaml.dump(OmegaConf.to_container(conf, resolve=True)))
 
 
 if __name__ == "__main__":
