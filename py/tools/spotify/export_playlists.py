c274ea498481bece127889c5fde363716a8ff886
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-04-28 22:16:38 -0700

    py: tools: spotify refactor (readability)

--- py/tools/spotify/export_playlists.py
+++ py/tools/spotify/export_playlists.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import os
+from functools import partial
 
 import tekore as tk
 
@@ -19,28 +20,30 @@ def load_spotify() -> tuple[str, tk.Spotify]:
     return user_id, spotify
 
 
-def query_all_batches(query, offset=0, total=None, *args, **kwargs):
-    x = query(*args, **kwargs, offset=offset)
+def query_all_batches(query, *, offset=0, total=None):
+    response = query(offset=offset)
     if total is None:
-        total = x.total
-    limit = x.limit
-    yield x
+        total = response.total
+    limit = response.limit
+    yield response
     if limit == 0:
         return
     for offset in range(offset + limit, total, limit):
-        yield query(*args, **kwargs, limit=limit, offset=offset)
+        response = query(limit=limit, offset=offset)
+        yield response
 
 
-def query_all(
-    query, iterate_batch=lambda batch: batch.items, args=(), **kwargs
-):
-    batches = query_all_batches(query, *args, **kwargs)
-    return [x for batch in batches for x in iterate_batch(batch)]
+def query_all(query, *, offset=0, total=None):
+    return (
+        item
+        for batch in query_all_batches(query, offset=offset, total=total)
+        for item in batch.items
+    )
 
 
 def main():
     user_id, spotify = load_spotify()
-    playlists = query_all(spotify.playlists, user_id=user_id)
+    playlists = query_all(partial(spotify.playlists, user_id=user_id))
     os.makedirs(PLAYLISTS_ROOT, exist_ok=True)
 
     for playlist in playlists:
@@ -49,7 +52,7 @@ def main():
         print(f"{filename}...")
 
         playlist_items = query_all(
-            spotify.playlist_items, playlist_id=playlist.id
+            partial(spotify.playlist_items, playlist_id=playlist.id)
         )
         tracks = [x.track for x in playlist_items]
         track_uris = [x.uri for x in tracks]
